664bdadc3dd0c234d377fbe22d276bbdd1963b83
Vadym Kinchur <vkincur@gmail.com>
2021-05-15 23:32:03 +0300

    Implement GitHub page object, fix element search in base PO

--- po/base.py
+++ po/base.py
@@ -1,6 +1,9 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class BasePO:
@@ -8,14 +11,27 @@ class BasePO:
         self.driver = driver
         self.url = url
 
+    def wait(self, timeout: int) -> None:
+        self.driver.implicitly_wait(timeout)
+
     def open_page(self) -> None:
         self.driver.get(self.url)
 
     def get_page_title(self) -> str:
         return self.driver.title
 
-    def get_element_by_css(self, selector: str) -> WebElement:
-        return self.driver.find_element(By.CSS, selector)
+    def get_page_url(self) -> str:
+        return self.driver.current_url
 
-    def get_element_by_xpath(self, xpath: str) -> WebElement:
-        return self.drivider.find_element(By.XPATH, xpath)
+    def __get_by_locator(self, locator: tuple[str, str], timeout: int) -> WebElement:
+        try:
+            wait = WebDriverWait(self.driver, timeout)
+            return wait.until(EC.presence_of_element_located(locator))
+        except TimeoutException:
+            raise Exception('Could not find element')
+
+    def get_by_css(self, selector: str, timeout: int = 0) -> WebElement:
+        return self.__get_by_locator((By.CSS_SELECTOR, selector), timeout)
+
+    def get_by_xpath(self, xpath: str, timeout: int = 0) -> WebElement:
+        return self.__get_by_locator((By.XPATH, xpath), timeout)
--- po/github.py
+++ po/github.py
@@ -1,7 +1,53 @@
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.common.keys import Keys
 from po.base import BasePO
 
 
 class GithubPO(BasePO):
     def __init__(self, driver: WebDriver) -> None:
         super().__init__(driver, 'https://github.com')
+
+    def search(self, query: str) -> None:
+        element = self.get_by_css('input[name="q"]')
+        element.send_keys(query)
+        element.send_keys(Keys.ENTER)
+
+    def get_related_topic(self) -> str:
+        selector = 'div.codesearch-results h3.mb-1'
+        element = self.get_by_css(selector)
+        return element.text
+
+    def __get_nth_repo(self, repo_number: int) -> WebElement:
+        selector = f'ul.repo-list :nth-child({repo_number})'
+        element = self.get_by_css(selector)
+        return element
+
+    def get_nth_repo_name(self, repo_number: int) -> str:
+        repo = self.__get_nth_repo(repo_number)
+        selector = 'a.v-align-middle'
+        name = repo.find_element(By.CSS_SELECTOR, selector)
+        return name.text
+
+    def get_nth_repo_language(self, repo_number: int) -> str:
+        repo = self.__get_nth_repo(repo_number)
+        selector = 'span[itemprop="programmingLanguage"]'
+        language = repo.find_element(By.CSS_SELECTOR, selector)
+        return language.text
+
+    def get_repo_count_from_menu(self) -> int:
+        selector = 'span.js-codesearch-count[data-search-type="Repositories"]'
+        element = self.get_by_css(selector)
+        return int(element.text)
+
+    def get_repo_count_from_search(self) -> int:
+        selector = 'div.codesearch-results h3:not(.mb-1)'
+        element = self.get_by_css(selector)
+        return int(element.text.split()[0])
+
+    def get_related_languages(self) -> list[str]:
+        lang_list = self.get_by_css('ul.filter-list')
+        lang_items = lang_list.find_elements(By.CSS_SELECTOR, 'a.filter-item')
+        languages = [item.text.split('\n')[1] for item in lang_items]
+        return languages
